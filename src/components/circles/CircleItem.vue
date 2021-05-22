f64adfeaffad3c335e55bbd377f89532567e8979
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-05-22 18:31:04 +0300

    Add images and display them in the component

---
+++ src/components/circles/CircleItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="itemsBox">
+    <div v-for="( item, i ) in items" :key="item.id" @click="selectWeapon(i)">
+      <section>{{ item.text }}</section>
+      <img :src="item.img" width="200px" height="200px" class="circle">
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+  methods: {
+    selectWeapon(i) {
+      console.log(i)
+    }
+  }
+}
+</script>
+
+<style>
+.circle {
+  background: transparent;
+  border: 2px red solid;
+  border-radius: 50%;
+  background-color: violet;
+  filter: drop-shadow(2px 4px 8px red);
+  transition: transform 0.5s;
+}
+
+.circle:hover {
+  background-color: teal;
+  transform: scale(1.1);
+}
+
+.itemsBox {
+  display: flex;
+  border: 2px solid black;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/data.js
@@ -0,0 +1,13 @@
+export const items = [{
+    id: 1,
+    text: "rock",
+    img: require('../assets/img/rock.png')
+}, {
+    id: 2,
+    text: "paper",
+    img: require('../assets/img/paper.png')
+}, {
+    id: 3,
+    text: "scissors",
+    img: require('../assets/img/makas.png')
+}, ]
